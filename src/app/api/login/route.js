0e92ce6309b3f8bddd45eaa1189dc59b2d1e3b67
Kelli Libin <km.putnam2@gmail.com>
2025-03-04 15:32:51 -0700

    remove logs

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -28,17 +28,13 @@ export async function POST(req, res) {
 
     // after successful login, get the user's uid and use that to look them up in user collection
     if (auth.currentUser) {
-  console.log('current user success')
       const uid = auth.currentUser.uid;
-      console.log('uid', uid)
       const userDoc = await getDoc(doc(db, "users", uid));
-console.log('user doc', userDoc)
+
       //providing that user exists, look them up to see if they are admin
       if (userDoc.exists()) {
-        console.log('exists')
         const userData = userDoc.data();
         if (userData.isAdmin) {
-          (console.log('is admin'))
           
           //set custom claims, which are then attached to the user object in firebase admin (admin true)
           await admin.auth()
