4e5e46e5265b49639aff39419e1dcf60072ab983
Kelli Libin <km.putnam2@gmail.com>
2025-03-05 13:03:09 -0700

    split components so fetching on server, fetch categories

--- src/app/createpost/page.jsx
+++ src/app/createpost/page.jsx
@@ -1,71 +1,22 @@
-"use client";
-import React, { useState } from "react";
-import styles from "./createPost.module.css";
-import Image from "next/image";
-import "react-quill/dist/quill.bubble.css";
-import ReactQuill from "react-quill";
+import React from "react";
+import CreatePost from "./CreatePost";
+import { getCategories } from "../firebase/queries/sectionQueries";
 
-const page = () => {
-  const [open, setOpen] = useState(false);
-  const [value, setValue] = useState("");
-  const [file, setFile] = useState(null);
-  const [title, setTitle] = useState("");
-  const [catSlug, setCatSlug] = useState("");
-  return (
-    <div className={styles.container}>
-      <input
-        type="text"
-        placeholder="Title"
-        className={styles.input}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <select
-        className={styles.select}
-        onChange={(e) => setCatSlug(e.target.value)}
-      >
-        <option value="style">style</option>
-        <option value="fashion">fashion</option>
-        <option value="food">food</option>
-        <option value="culture">culture</option>
-        <option value="travel">travel</option>
-        <option value="coding">coding</option>
-      </select>
-      <div className={styles.editor}>
-        <button className={styles.button} onClick={() => setOpen(!open)}>
-          <Image src="/plus.png" alt="" width={16} height={16} />
-        </button>
-        {open && (
-          <div className={styles.add}>
-            <input
-              type="file"
-              id="image"
-              onChange={(e) => setFile(e.target.files[0])}
-              style={{ display: "none" }}
-            />
-            <button className={styles.addButton}>
-              <label htmlFor="image">
-                <Image src="/image.png" alt="" width={16} height={16} />
-              </label>
-            </button>
-            <button className={styles.addButton}>
-              <Image src="/external.png" alt="" width={16} height={16} />
-            </button>
-            <button className={styles.addButton}>
-              <Image src="/video.png" alt="" width={16} height={16} />
-            </button>
-          </div>
-        )}
-        <ReactQuill
-          className={styles.textArea}
-          theme="bubble"
-          value={value}
-          onChange={setValue}
-          placeholder="Tell your story..."
-        />
-      </div>
-      <button className={styles.publish}>Publish</button>
-    </div>
-  );
-};
+async function page() {
+  let categories = null;
+  let error = null;
+
+  try {
+    const categoriesInDB = await getCategories();
+    if (Array.isArray(categoriesInDB) && categoriesInDB.length > 1) {
+      categories = categoriesInDB;
+    }
+  } catch (err) {
+    error = "an error has occurred fetching categories";
+
+  }
+
+  return <CreatePost categories={categories} error={error} />;
+}
 
 export default page;
