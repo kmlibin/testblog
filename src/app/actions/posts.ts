12e86706b3b1f42cf1cafd4d9e7505fcc410d52d
Kelli Libin <km.putnam2@gmail.com>
2025-03-10 14:47:43 -0600

    finish create post functionality - adding post ids to category collection as well

--- src/app/actions/posts.ts
+++ src/app/actions/posts.ts
@@ -1,11 +1,18 @@
-"use server"
+"use server";
 
-
-
-import {collection, addDoc, getDoc, updateDoc, doc, deleteDoc } from 'firebase/firestore/lite'
-import { db, storage } from '../firebase/config'
+import {
+  collection,
+  addDoc,
+  getDoc,
+  updateDoc,
+  doc,
+  deleteDoc,
+  arrayUnion,
+} from "firebase/firestore/lite";
+import { db, storage } from "../firebase/config";
 import { ref, deleteObject } from "firebase/storage";
-import { BlogPost } from '../types';
+import { BlogPost } from "../types";
+import { revalidatePath } from "next/cache";
 
 interface SuccessResponse {
   error: false;
@@ -20,13 +27,59 @@ interface ErrorResponse {
 
 type CreateBlogPostResponse = SuccessResponse | ErrorResponse;
 
-export async function createBlogPost(post: BlogPost): Promise<CreateBlogPostResponse> {
-    try {
- 
-      const docRef = await addDoc(collection(db, "posts"), post);
-      return { error: false, message: "post successfully created", id: docRef.id };
-    } catch (error) {
-      console.error('Error adding post to Firestore:', error);
-      return { error: true, message: "error adding post to firestore" };
+export async function createBlogPost(
+  post: BlogPost
+): Promise<CreateBlogPostResponse> {
+  const {
+    title,
+    additionalImages,
+    coverImage,
+    content,
+    date,
+    draft,
+    slug,
+    views,
+    tags,
+    category,
+  } = post;
+  //create variable so can pass back id to frontend success return
+  let newId;
+  try {
+    //add the post to the db
+    const newPost = await addDoc(collection(db, "posts"), post);
+    //if it exists, update the id variable, then add it to the appropriate category
+    if (newPost.id) {
+      newId = newPost.id;
+      const categoryDocRef = doc(db, "categories", category);
+      const categoryDocSnapshot = await getDoc(categoryDocRef);
+
+      if (categoryDocSnapshot.exists()) {
+        const updatedCategoryData = {
+          posts: arrayUnion(newPost.id),
+        };
+        await updateDoc(categoryDocRef, updatedCategoryData);
+      } else {
+        console.log("section does not exist");
+      }
     }
+  } catch (error: any) {
+    //return errors, then successes
+    if (error.code === "permission-denied") {
+      return {
+        error: true,
+        message: "Permission denied. Please try again later.",
+      };
+    }
+    return {
+      error: true,
+      message: "Error adding post to database, try again later",
+    };
   }
+  //revalidate paths when you figure out rest of pages
+  revalidatePath("/");
+  return {
+    error: false,
+    message: "Successfully added post to database!",
+    id: newId ? newId : "/",
+  };
+}
