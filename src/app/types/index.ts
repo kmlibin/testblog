6bf7c312bf6ce8d58a8655e20a467753f1af317a
Kelli Libin <km.putnam2@gmail.com>
2025-03-10 16:37:18 -0600

    format, add type for blog post with id

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -1,40 +1,57 @@
 import { Timestamp } from "firebase/firestore/lite";
 
 export type ImageFile = {
+  url: string;
+  path: string;
+};
+
+export interface CategoryWithId {
+  id: string;
+  color: string;
+  image: {
+    path: string;
     url: string;
-   path: string;
   };
-  
-export interface CategoryWithId{
-    id: string;
-    color: string;
-    image: {
-        path: string,
-        url: string
-    };
-    name: string;
-    posts: string[]
+  name: string;
+  posts: string[];
 }
 
 export interface Category {
-    color: string;
-    image: {
-        path: string,
-        url: string
-    };
-    name: string;
-    posts: string[]
+  color: string;
+  image: {
+    path: string;
+    url: string;
+  };
+  name: string;
+  posts: string[];
 }
 
 export interface BlogPost {
-    additionalImages: ImageFile[],
+  additionalImages: ImageFile[];
+  category: string;
+  content: string;
+  coverImage: ImageFile;
+  date: Date;
+  draft: boolean;
+  slug: string;
+  tags: string[];
+  title: string;
+  views: number;
+}
+
+export interface BlogPostWithId {
+  id: string;
+  data: {
+    additionalImages: ImageFile[];
     category: string;
     content: string;
-    coverImage: ImageFile,
+    coverImage: ImageFile;
     date: Date;
     draft: boolean;
     slug: string;
     tags: string[];
     title: string;
     views: number;
+  };
 }
+
