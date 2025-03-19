ee5d21a26626d0f9786fe8c8904fd24f62be9a56
Kelli Libin <km.putnam2@gmail.com>
2025-03-19 15:23:41 -0600

    receive props, use, send to children. also update to category color is dynamic

--- src/app/[singlePageSlug]/SinglePost.tsx
+++ src/app/[singlePageSlug]/SinglePost.tsx
@@ -1,55 +1,60 @@
-'use client'
+"use client";
 import React from "react";
 import Menu from "@/components/menu/Menu";
 import styles from "./singlePage.module.css";
 import Image from "next/image";
-import { BlogPostWithId, BlogPost } from "../types";
+import { BlogPostWithId, BlogPost, CategoryWithId } from "../types";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.bubble.css";
+import { formatDate } from "../utils/formatDate";
+
 type SinglePostProps = {
   blogPostError: string | null;
-  blogPost: BlogPostWithId | null ;
+  blogPost: BlogPostWithId | null;
+  myPicks: BlogPostWithId[] | null;
+  myPicksError: string | null;
+  popularPostsError: string | null;
+  popularPosts: BlogPostWithId[] | null;
+  categories: CategoryWithId[] | null;
+  categoriesError: string | null;
 };
 
-const SinglePost = ({ blogPostError, blogPost }: SinglePostProps) => {  
-
-    if (blogPostError || !blogPost) {
+const SinglePost = ({
+  blogPostError,
+  blogPost,
+  myPicks,
+  myPicksError,
+  popularPosts,
+  popularPostsError,
+  categories,
+  categoriesError,
+}: SinglePostProps) => {
+  if (blogPostError || !blogPost) {
     return <div className={styles.error}>Error fetching Post...</div>;
   }
-console.log(blogPost)
-    const {
-      title,
-      content,
-      categoryName,
-      category,
-      tags,
-      date,
-      draft,
-      editedAt,
-      coverImage,
-      additionalImages,
-    } = blogPost?.data;
+  const {
+    title,
+    content,
+    categoryName,
+    categoryColor,
+    category,
+    tags,
+    date,
+    draft,
+    editedAt,
+    coverImage,
+    additionalImages,
+  } = blogPost?.data;
 
+  const formattedDate = formatDate(date ? date : null);
+  const formattedEditedDate = formatDate(editedAt ? editedAt : null);
 
-const formattedDate = date ? new Date(date).toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric"
-}) : "Invalid Date"
-
-const formattedEditedDate = editedAt? new Date(editedAt).toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric"
-}) : "Invalid Date"
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.textContainer}>
-          <h1 className={styles.title}>
-            {title}
-          </h1>
-          <h4 className={styles.catName}>{categoryName}</h4>
+          <h1 className={styles.title}>{title}</h1>
+          <h4 className={styles.catName} style={{backgroundColor: `${categoryColor}`}}>{categoryName}</h4>
           <div className={styles.user}>
             <div className={styles.userImageContainer}>
               <Image src="/p1.jpeg" alt="" fill className={styles.userImage} />
@@ -57,21 +62,37 @@ const formattedEditedDate = editedAt? new Date(editedAt).toLocaleDateString("en-
             <div className={styles.userTextContainer}>
               <span className={styles.username}>Author Name</span>
               <span className={styles.date}>{formattedDate}</span>
-              {editedAt? (<span className={styles.editedAt}>Last Edited: {formattedEditedDate}</span>) : null}
+              {editedAt ? (
+                <span className={styles.editedAt}>
+                  Last Edited: {formattedEditedDate}
+                </span>
+              ) : null}
             </div>
           </div>
         </div>
         <div className={styles.imageContainer}>
-          <Image alt="" fill src={coverImage? coverImage.url : '/p1.jpeg'} className={styles.image} />
+          <Image
+            alt=""
+            fill
+            src={coverImage ? coverImage.url : "/p1.jpeg"}
+            className={styles.image}
+          />
         </div>
       </div>
       <div className={styles.content}>
         <div className={styles.post}>
           <div className={styles.description}>
-        <ReactQuill value={content} readOnly theme={"bubble"}/>
+            <ReactQuill value={content} readOnly theme={"bubble"} />
           </div>
         </div>
-        <Menu />
+        <Menu
+          myPicks={myPicks}
+          myPicksError={myPicksError}
+          popularPosts={popularPosts}
+          popularPostsError={popularPostsError}
+          categories={categories}
+          categoriesError={categoriesError}
+        />
       </div>
     </div>
   );
