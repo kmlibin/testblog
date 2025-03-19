d072eded051aca0ee619e2271b77630198e6356e
Kelli Libin <km.putnam2@gmail.com>
2025-03-19 15:23:19 -0600

    fetch popular posts, editor picks, categories, and blogposts on this page to pass down tochildren

--- src/app/[singlePageSlug]/page.tsx
+++ src/app/[singlePageSlug]/page.tsx
@@ -4,7 +4,10 @@ import { getBlogPostById } from "../firebase/queries/blogPostQueries";
 import SinglePost from "./SinglePost";
 import styles from "./singlePage.module.css";
 import { addCount } from "../actions/posts";
-
+import { getPopularPosts } from "../firebase/queries/blogPostQueries";
+import { getPicks } from "../firebase/queries/picksQueries";
+import { getCategories } from "../firebase/queries/sectionQueries";
+import { CategoryWithId } from "../types";
 type Props = {
   searchParams: {
     id: string;
@@ -18,24 +21,62 @@ async function page({ searchParams, params }: Props) {
   const postId = searchParams.id;
   let blogPost: BlogPostWithId | null = null;
   let blogPostError: string | null = null;
+  let myPicks: BlogPostWithId[] | null = null;
+  let myPicksError: string | null = null;
+  let popularPosts: BlogPostWithId[] | null = null;
+  let popularPostsError: string | null = null;
+  let categoriesError: string | null = null;
+  let categories: CategoryWithId[] | null = null;
 
+  //get categories for menu
+  const categoryResult = await getCategories();
+  if (categoryResult.error) {
+    categoriesError = categoryResult.error;
+  } else {
+    categories = categoryResult.categories;
+  }
 
-  try {
-    //add to count
+  //get popular posts for menu
+  const getPosts = await getPopularPosts();
+  if (getPosts.error) {
+    popularPostsError = getPosts.error;
+  } else {
+    popularPosts = getPosts.data;
+  }
+
+  //get editor picks for menu
+  const getMyPicks = await getPicks();
+  if (getMyPicks.error) {
+    myPicksError = getMyPicks.error;
+  } else {
+    myPicks = getMyPicks.data;
+  }
+
+  //get blogpost
+  const blogPostResult = await getBlogPostById(postId);
+  if (blogPostResult.error) {
+    blogPostError = blogPostResult.error;
+  } else {
+    blogPost = blogPostResult.data;
+  }
+
+  //add to count
+  if (blogPost?.data?.draft === false) {
     await addCount(postId);
-    const blogPostResult = await getBlogPostById(postId);
-    if (blogPostResult.error) {
-      blogPostError = blogPostResult.error;
-    } else {
-      blogPost = blogPostResult.data;
-    }
-  } catch (error) {
-    blogPostError = "Error fetching post";
   }
 
   return (
     <div className={styles.container}>
-      <SinglePost blogPost={blogPost} blogPostError={blogPostError} />
+      <SinglePost
+        blogPost={blogPost}
+        blogPostError={blogPostError}
+        myPicks={myPicks}
+        popularPosts={popularPosts}
+        popularPostsError={popularPostsError}
+        myPicksError={myPicksError}
+        categories={categories}
+        categoriesError={categoriesError}
+      />
     </div>
   );
 }
