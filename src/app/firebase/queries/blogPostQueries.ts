f56f1ee974b4e01627d030e8e211f0a239a3f85d
Kelli Libin <km.putnam2@gmail.com>
2025-03-19 12:33:49 -0600

    added function for retrieving popular posts, sorts posts by views and returns five

--- src/app/firebase/queries/blogPostQueries.ts
+++ src/app/firebase/queries/blogPostQueries.ts
@@ -11,18 +11,19 @@ import {
   count,
   getCount,
   startAt,
-  Timestamp
+  Timestamp,
 } from "firebase/firestore/lite";
-
 
 import { db } from "@/app/firebase/config";
 import { BlogPost, Category } from "@/app/types";
 import { BlogPostWithId } from "@/app/types";
 
-type BlogPostResult = { data: BlogPostWithId | null; error: string | null };
+type SingleBlogPostResult = { data: BlogPostWithId | null; error: string | null };
+type MultipleBlogPostResult = { data: BlogPostWithId[] | null; error: string | null };
 
-export async function getBlogPostById(id: string): Promise<BlogPostResult> {
+export async function getBlogPostById(id: string): Promise<SingleBlogPostResult> {
   const postRef = doc(db, "posts", id);
+
   try {
     const postSnapshot = await getDoc(postRef);
 
@@ -32,8 +33,54 @@ export async function getBlogPostById(id: string): Promise<BlogPostResult> {
     if (postSnapshot) {
       const postData = postSnapshot.data() as BlogPost;
 
-        // convert Firestore timestamps to plain JavaScript Date or string
-        const formattedPostData = {
+      // convert Firestore timestamps to plain JavaScript Date or string
+      const formattedPostData = {
+        ...postData,
+        date:
+          postData.date instanceof Timestamp
+            ? postData.date.toDate().toISOString()
+            : null,
+        editedAt:
+          postData.editedAt instanceof Timestamp
+            ? postData.editedAt.toDate().toISOString()
+            : null,
+      };
+
+      return {
+        data: {
+          id: postSnapshot.id,
+          data: formattedPostData,
+        },
+        error: null,
+      };
+    }
+    return { data: null, error: "Error fetching post" };
+  } catch (error: any) {
+    return { data: null, error: error.message };
+  }
+}
+
+export async function getPopularPosts(): Promise<MultipleBlogPostResult> {
+  try {
+    // query the posts collection, ordering by views in descending order and limiting to 5
+    const postsQuery = query(
+      collection(db, "posts"),
+      orderBy("views", "desc"),
+      limit(5)
+    );
+    const postsSnapshot = await getDocs(postsQuery);
+
+    if (postsSnapshot.empty) {
+      return { data: null, error: "No popular posts found." };
+    }
+
+    // format each post
+    const popularPosts: BlogPostWithId[] = postsSnapshot.docs.map((doc) => {
+      const postData = doc.data() as BlogPost;
+
+      return {
+        id: doc.id,
+        data: {
           ...postData,
           date:
             postData.date instanceof Timestamp
@@ -43,17 +90,11 @@ export async function getBlogPostById(id: string): Promise<BlogPostResult> {
             postData.editedAt instanceof Timestamp
               ? postData.editedAt.toDate().toISOString()
               : null,
-        };
-
-      return {
-        data: {
-          id: postSnapshot.id,
-          data: formattedPostData ,
         },
-        error: null,
       };
-    }
-    return { data: null, error: "Error fetching post" };
+    });
+
+    return { data: popularPosts, error: null };
   } catch (error: any) {
     return { data: null, error: error.message };
   }
