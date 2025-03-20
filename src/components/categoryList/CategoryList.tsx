492101f6af4d83ac51b6c814e73b2019317c7a80
Kelli Libin <km.putnam2@gmail.com>
2025-03-20 15:52:38 -0600

    now dynamic and hooked to db

--- src/components/categoryList/CategoryList.tsx
+++ src/components/categoryList/CategoryList.tsx
@@ -2,67 +2,44 @@ import React from "react";
 import styles from "./categorylist.module.css";
 import Link from "next/link";
 import Image from "next/image";
-const CategoryList = () => {
+import { CategoryWithId } from "@/app/types";
+import paths from "@/paths";
+
+type CategoryListProps = {
+  withImage: boolean;
+  categories: CategoryWithId[] | null;
+  categoriesError: string | null;
+};
+const CategoryList = ({
+  withImage,
+  categories,
+  categoriesError,
+}: CategoryListProps) => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Popular Categories</h1>
-      <div className={styles.categories}>
-      
-          <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
-            <Image
-              src="/style.png"
-              alt="style"
-              width={32}
-              height={32}
-              className={styles.image}
-            />
-            Travel
-          </Link>
-
-          <Link href="/blog?cat=style" className={`${styles.category} ${styles.coding}`}>
-            <Image
-              src="/coding.png"
-              alt="style"
-              width={32}
-              height={32}
-              className={styles.image}
-            />
-            Coding
-          </Link>
-
-          <Link href="/blog?cat=style" className={`${styles.category} ${styles.food}`}>
-            <Image
-              src="/food.png"
-              alt="style"
-              width={32}
-              height={32}
-              className={styles.image}
-            />
-           Cooking
-          </Link>
-
-          <Link href="/blog?cat=style" className={`${styles.category} ${styles.garden}`}>
-            <Image
-              src="/fashion.png"
-              alt="style"
-              width={32}
-              height={32}
-              className={styles.image}
-            />
-            Garden
-          </Link>
-
-          <Link href="/blog?cat=style" className={`${styles.category} ${styles.other}`}>
-            <Image
-              src="/travel.png"
-              alt="style"
-              width={32}
-              height={32}
-              className={styles.image}
-            />
-            Other
-          </Link>
-    
+      <div className={styles.categoryList}>
+       
+          {categoriesError && "Error fetching categories..."}
+          {categories?.map((cat) => (
+            <Link
+              href={paths.viewCategoryPage(cat.name, cat.id)}
+              className={styles.categoryItem}
+              style={{ backgroundColor: cat.color }}
+              key={cat.id}
+            >
+              {withImage && (
+                <Image
+                  src="/style.png"
+                  alt="style"
+                  width={32}
+                  height={32}
+                  className={styles.image}
+                />
+              )}
+              <span className={styles.catTitle}>{cat.name}</span>
+            </Link>
+          ))}
       </div>
     </div>
   );
--- src/components/featured/Featured.tsx
+++ src/components/featured/Featured.tsx
@@ -1,26 +1,49 @@
 import React from "react";
 import styles from "./featured.module.css";
 import Image from "next/image";
-const Featured = () => {
+import { BlogPostWithId } from "@/app/types";
+import truncateHTMLText from "@/app/utils/truncateText";
+import Link from "next/link";
+import paths from "@/paths";
+
+type FeaturedProps = {
+  featured: BlogPostWithId | null;
+  featuredError: string | null;
+};
+const Featured = ({ featured, featuredError }: FeaturedProps) => {
+  let descriptionText = "";
+  if (featured?.data.content) {
+    descriptionText = truncateHTMLText(featured?.data?.content, 25);
+  }
+  //if featured is null or if featured error exists, return no content
+  if (!featured) return null;
+
+  if (featuredError) return null;
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
-        <b>Hey, it's me!</b> 
+        <b>Hey, it's me!</b>
         Discover my stories and adventures
       </h1>
       <div className={styles.post}>
         <div className={styles.imgContainer}>
-          <Image src="/p1.jpeg" alt="image" fill className={styles.image}/>
+          <Image
+            src={featured?.data?.coverImage?.url || "/p1.jpeg"}
+            alt="image"
+            fill
+            className={styles.image}
+          />
         </div>
         <div className={styles.textContainer}>
-          <h1 className={styles.postTitle}>Lorem ipsum dolor sit amet,</h1>
-          <p className={styles.postDescription}>
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.{" "}
-          </p>
-          <button className={styles.button}>Read More</button>
+          <h1 className={styles.postTitle}>{featured?.data.title}</h1>
+          <p className={styles.postDescription}>{descriptionText}</p>
+          <Link
+            href={paths.viewSinglePostPage(featured?.data?.slug, featured?.id)}
+            className={styles.featuredLink}
+          >
+            Read More
+          </Link>
         </div>
       </div>
     </div>
