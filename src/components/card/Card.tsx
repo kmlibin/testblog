5c094cc25c0f0c6f11376817b76e175d0ea2dc3b
Kelli Libin <km.putnam2@gmail.com>
2025-03-20 15:52:17 -0600

    now displays dynamically

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,29 +2,45 @@ import React from "react";
 import styles from "./card.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import { BlogPostWithId } from "@/app/types";
+import { formatDate } from "@/app/utils/formatDate";
+import truncateHTMLText from "@/app/utils/truncateText";
+import paths from "@/paths";
 
-const Card = () => {
+
+type CardProps = {
+  post: BlogPostWithId;
+}
+const Card = ({post}:CardProps) => {
+  let descriptionText
+  let formattedDate
+  if(post && post.data.date) {
+    formattedDate = formatDate(post.data.date)
+  }
+
+  if(post) {
+    descriptionText = truncateHTMLText(post.data.content, 25)
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
-        <Image src="/p1.jpeg" alt="image" fill className={styles.image} />
+        <Image src={post?.data?.coverImage?.url || "/p1.jpeg"} alt="image" fill className={styles.image} />
       </div>
       <div className={styles.textContainer}>
         <div className={styles.detail}>
-          <span className={styles.date}>11.11.2024 - </span>
-          <span className={styles.category}>CODING</span>
+          <span className={styles.date}>{formattedDate} - </span>
+          <span className={styles.category}>{post.data.categoryName}</span>
         </div>
-        <Link href="/">
+        <Link href={paths.viewSinglePostPage(post.data.slug, post.id)}>
           <h1 className={styles.title}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+           {post.data.title}
           </h1>
         </Link>
         <p className={styles.desc}>
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-          enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-          ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          {descriptionText}
         </p>
-        <Link href="/" className={styles.link}>
+        <Link href={paths.viewSinglePostPage(post.data.slug, post.id)} className={styles.link}>
           Read More
         </Link>
       </div>
--- src/components/cardList/CardList.tsx
+++ src/components/cardList/CardList.tsx
@@ -1,20 +1,30 @@
+
 import React from "react";
 import styles from "./cardlist.module.css";
 import Pagination from "../pagination/Pagination";
-import Image from "next/image";
 import Card from "../card/Card";
-const CardList = () => {
+import { BlogPostWithId } from "@/app/types";
+
+type CardListProps = {
+  blogPosts: BlogPostWithId[] | null;
+  blogPostsError: string | null;
+  currentPage: number,
+  totalPages: number,
+}
+
+const CardList = ({blogPosts, blogPostsError, currentPage, totalPages}: CardListProps) => {
+  if(blogPostsError || !blogPosts) {
+    return <div>{blogPostsError}</div>
+  }
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
       <div className={styles.posts}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {blogPosts.map((post) => (
+          <Card key={post.id} post = {post} />
+        ))}
       </div>
-      <Pagination />
+      <Pagination currentPage={currentPage} totalPages={totalPages} />
     </div>
   );
 };
