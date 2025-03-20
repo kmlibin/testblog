f08fe83c2e42b5727c1ead2f0b8b8832fd7962b6
Kelli Libin <km.putnam2@gmail.com>
2025-03-20 15:53:40 -0600

    hooked up basic pagination

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,10 +1,42 @@
+'use client'
 import React from "react";
 import styles from "./pagination.module.css";
-const Pagination = () => {
+import { useRouter, useParams } from "next/navigation";
+
+type PaginationProps = {
+  currentPage: number;
+  totalPages: number;
+}
+
+const Pagination = ({currentPage, totalPages} : PaginationProps) => {
+  const router = useRouter()
+  const params = useParams()
+
+  const handlePageChange = (page: number) => {
+    if (totalPages !== undefined) {
+      if (page > 0 && page <= totalPages) {
+        router.push(`/?page=${page}`);
+      }
+    }
+  };
+
   return (
     <div className={styles.container}>
-      <button className={styles.button}>Previous</button>
-      <button className={styles.button}>Next</button>
+           <button className={styles.button}
+        onClick={() => handlePageChange(currentPage - 1)}
+        disabled={currentPage <= 1}
+      >
+        Previous
+      </button>
+      <span>
+        Page {currentPage} of {totalPages}
+      </span>
+      <button
+        onClick={() => handlePageChange(currentPage + 1)}
+        disabled={currentPage >= totalPages}
+      >
+        Next
+      </button>
     </div>
   );
 };
