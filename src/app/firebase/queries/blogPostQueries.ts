4b292486f900256926b451aaa5e58c02f41237e0
Kelli Libin <km.putnam2@gmail.com>
2025-03-20 15:50:52 -0600

    new function for fetching active posts, has pagination as well. removed checks for drafts in other functions since drafts now its own collection

--- src/app/firebase/queries/blogPostQueries.ts
+++ src/app/firebase/queries/blogPostQueries.ts
@@ -15,7 +15,7 @@ import {
 } from "firebase/firestore/lite";
 
 import { db } from "@/app/firebase/config";
-import { BlogPost, Category } from "@/app/types";
+import { BlogPost } from "@/app/types";
 import { BlogPostWithId } from "@/app/types";
 
 type SingleBlogPostResult = {
@@ -27,10 +27,133 @@ type MultipleBlogPostResult = {
   error: string | null;
 };
 
+
+// gets prev document for pagination
+async function getLastDocument(page: number, pageLimit: number) {
+  // fetch documents when page > 1 (because we need a last document to continue the query)
+  console.log("page", page, "pageLimit", pageLimit, "limitBy", pageLimit * (page - 1))
+  if (page > 1) {
+    const postsQuery = query(
+      collection(db, "posts"),
+      orderBy("date", "desc"),
+      limit(pageLimit * (page - 1)) 
+    );
+    const postsSnapshot = await getDocs(postsQuery);
+
+    // last document on the current page
+    const lastDocument = postsSnapshot.docs[postsSnapshot.docs.length - 1];
+    console.log("last document:", lastDocument ? lastDocument.data() : "No last document found");
+
+    return lastDocument || null;
+  }
+
+  // if it's the first page, no last document is needed - return null
+  return null;
+}
+
+//limit how many to show per page, start after doc is the doc you want it to start after, last visible stores last doc of current page
+export async function getActiveBlogPosts(
+  page: number,
+  limitCount: number,
+): Promise<{
+  error: string | null;
+  data: BlogPostWithId[] | null;
+  totalPages: number | null;
+  totalPosts: number | null;
+}> {
+
+  console.log("page", page, "limitCount", limitCount)
+
+  try {
+    //start document for pagination
+    let postsQuery = query(
+      collection(db, "posts"),
+      orderBy("date", "desc"),
+      limit(limitCount)
+    );
+
+    // get the last document from the previous page
+    if (page > 1) {
+      const lastDocument = await getLastDocument(page, limitCount);
+
+      if (!lastDocument) {
+        console.log("Error: No valid last document found");
+        return {
+          error: "Error fetching posts",
+          data: null,
+          totalPages: null,
+          totalPosts: null,
+        };
+      }
+      postsQuery = query(
+        collection(db, "posts"),
+        orderBy("date", "desc"),
+        startAfter(lastDocument),
+        limit(limitCount)
+      );
+    }
+
+    //access the documents in the db database
+    const postsSnapshot = await getDocs(postsQuery);
+    //get count from database
+    const totalPostsSnapshot = await getCount(collection(db, "posts"));
+    const totalPosts = totalPostsSnapshot.data().count;
+    //calculate total pages
+    const totalPages = Math.ceil(totalPosts / limitCount);
+    if (postsSnapshot.empty) {
+      return {
+        error: "error fetching posts",
+        data: null,
+        totalPages: 0,
+        totalPosts: 0,
+      };
+    }
+
+    if (postsSnapshot) {
+      const posts: any = postsSnapshot.docs
+        .map((doc) => {
+          const postData = doc.data() as BlogPost;
+          const formattedPostData = {
+            ...postData,
+            date:
+              postData.date instanceof Timestamp
+                ? postData.date.toDate().toISOString()
+                : null,
+            editedAt:
+              postData.editedAt instanceof Timestamp
+                ? postData.editedAt.toDate().toISOString()
+                : null,
+          };
+          return { id: doc.id, data: formattedPostData };
+        })
+    
+
+      return { error: null, data: posts, totalPages, totalPosts };
+    }
+    return {
+      error: "Error fetching posts",
+      data: null,
+      totalPages: null,
+      totalPosts: null,
+    };
+  } catch (error: any) {
+    return {
+      error: error.message,
+      data: null,
+      totalPages: null,
+      totalPosts: null,
+    };
+  }
+}
+
 export async function getBlogPostById(
-  id: string
+  id: string, 
+  draft: string,
 ): Promise<SingleBlogPostResult> {
-  const postRef = doc(db, "posts", id);
+
+  console.log("draft", draft, "id", id)
+  const collection = draft === "true" ? "drafts" : "posts"
+  const postRef = doc(db, collection, id);
 
   try {
     const postSnapshot = await getDoc(postRef);
@@ -88,9 +211,6 @@ export async function getPopularPosts(): Promise<MultipleBlogPostResult> {
       .map((doc) => {
         const postData = doc.data() as BlogPost;
         //don't return it if it's a draft
-        if (postData.draft) {
-          return null;
-        }
         return {
           id: doc.id,
           data: {
