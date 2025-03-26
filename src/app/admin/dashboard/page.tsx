1b6e1cfee5516d5e6ee185701a21442617029970
Kelli Libin <km.putnam2@gmail.com>
2025-03-26 11:58:45 -0600

    delete blogpost fetch

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -5,69 +5,66 @@ import { getCategories } from "@/app/firebase/queries/sectionQueries";
 import { getPicks } from "@/app/firebase/queries/picksQueries";
 import getFeatured from "@/app/firebase/queries/featuredQueries";
 
-
 type Props = {
   searchParams: {
     date: string;
     draft: string;
     category: string;
-
   };
 };
-
 
 //this page should be shown only to admin, needs an auth check
 // default loads active posts, but tabs at top show "drafts" or "active posts"
 //should be able to sort by date, category.
 
-async function page ({searchParams}: Props) {
+async function page({ searchParams }: Props) {
+  // console.log(searchParams);
+  let blogPost: BlogPostWithId | null = null;
+  let blogPostError: string | null = null;
+  let myPicks: BlogPostWithId[] | null = null;
+  let myPicksError: string | null = null;
+  let featuredPost: BlogPostWithId | null = null;
+  let featuredPostError: string | null = null;
+  let categoriesError: string | null = null;
+  let categories: CategoryWithId[] | null = null;
 
-    
-  
-    console.log(searchParams);
-    let blogPost: BlogPostWithId | null = null;
-    let blogPostError: string | null = null;
-    let myPicks: BlogPostWithId[] | null = null;
-    let myPicksError: string | null = null;
-    let featuredPost: BlogPostWithId | null = null;
-    let featuredPostError: string | null = null;
-    let categoriesError: string | null = null;
-    let categories: CategoryWithId[] | null = null;
+  //get categories for menu
+  const categoryResult = await getCategories();
+  if (categoryResult.error) {
+    categoriesError = categoryResult.error;
+  } else {
+    categories = categoryResult.categories;
+  }
 
-    console.log(searchParams);
-    //get categories for menu
-    const categoryResult = await getCategories();
-    if (categoryResult.error) {
-      categoriesError = categoryResult.error;
-    } else {
-      categories = categoryResult.categories;
-    }
-  
-    //get editor picks for menu
-    const getMyPicks = await getPicks();
-    if (getMyPicks.error) {
-      myPicksError = getMyPicks.error;
-    } else {
-      myPicks = getMyPicks.data;
-    }
+  //get editor picks 
+  const getMyPicks = await getPicks();
+  if (getMyPicks.error) {
+    myPicksError = getMyPicks.error;
+  } else {
+    myPicks = getMyPicks.data;
+  }
+//get featured post
+  const featured = await getFeatured();
+  // console.log(featured);
+  if (featured.error) {
+    featuredPostError = featured.error;
+  } else {
+    featuredPost = featured.data;
+  }
 
-    const featured = await getFeatured();
-    console.log(featured)
-    if(featured.error) {
-      featuredPostError = featured.error
-    } else {
-      featuredPost = featured.data
-    }
-  
-    //get blogpost
-    // const blogPostResult = await getBlogPostById(postId, searchParams.draft);
-    // if (blogPostResult.error) {
-    //   blogPostError = blogPostResult.error;
-    // } else {
-    //   blogPost = blogPostResult.data;
-    // }
-  
-  return <div><AdminDashboard myPicks={myPicks} myPicksError={myPicksError} featuredPost={featuredPost} featuredPostError={featuredPostError} categories= {categories} categoriesError={categoriesError}/></div>;
-};
+
+  return (
+    <div>
+      <AdminDashboard
+        myPicks={myPicks}
+        myPicksError={myPicksError}
+        featuredPost={featuredPost}
+        featuredPostError={featuredPostError}
+        categories={categories}
+        categoriesError={categoriesError}
+      />
+    </div>
+  );
+}
 
 export default page;
