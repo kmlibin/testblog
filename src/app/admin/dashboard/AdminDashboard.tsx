f7c053c95006a5610070ed14782682a78607ba87
Kelli Libin <km.putnam2@gmail.com>
2025-03-26 11:58:34 -0600

    adding table card

--- src/app/admin/dashboard/AdminDashboard.tsx
+++ src/app/admin/dashboard/AdminDashboard.tsx
@@ -7,11 +7,12 @@ import { useSearchParams } from "next/navigation";
 import { getActiveBlogPosts } from "@/app/firebase/queries/blogPostQueries";
 import { useRouter } from "next/navigation";
 import { usePathname } from "next/navigation";
-
+import { getCategories } from "@/app/firebase/queries/sectionQueries";
+import { getPicks } from "@/app/firebase/queries/picksQueries";
+import getFeatured from "@/app/firebase/queries/featuredQueries";
+import TableCard from "./TableCard";
 
 type AdminDashboardProps = {
-  // blogPostError: string | null;
-  // blogPost: BlogPostWithId | null;
   myPicks: BlogPostWithId[] | null;
   myPicksError: string | null;
   featuredPostError: string | null;
@@ -20,7 +21,7 @@ type AdminDashboardProps = {
   categoriesError: string | null;
 };
 
-export  function debounce(func: any, wait: any) {
+export function debounce(func: any, wait: any) {
   let timeout: any;
   return function (...args: any) {
     clearTimeout(timeout);
@@ -28,7 +29,6 @@ export  function debounce(func: any, wait: any) {
     timeout = setTimeout(() => func.apply(this, args), wait);
   };
 }
-
 
 const AdminDashboard = ({
   myPicks,
@@ -38,57 +38,72 @@ const AdminDashboard = ({
   categories,
   categoriesError,
 }: AdminDashboardProps) => {
-  const searchParams = useSearchParams()
-  const router = useRouter()
-  const pathname = usePathname()
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
 
   const [activeTab, setActiveTab] = useState<string>("posts");
   const [sortOrder, setSortOrder] = useState<string>("desc");
   const [category, setCategory] = useState<string>("all");
-  const [currentPage, setCurrentPage] = useState(1)
-  const [limitCount, setLimitCount] = useState(5)
-  
+  const [currentPage, setCurrentPage] = useState(1);
+  const [limitCount, setLimitCount] = useState(5);
+  const [posts, setPosts] = useState<BlogPostWithId[] | null>(null);
+  const [postsError, setPostsError] = useState<string | null>(null);
+
   const createQueryString = useCallback(
     (name: string, value: string) => {
-      const params = new URLSearchParams(searchParams.toString())
-      params.set(name, value)
- 
-      return params.toString()
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
+
+      return params.toString();
     },
     [searchParams]
-  )
+  );
 
-  console.log(searchParams)
+  console.log(searchParams);
   useEffect(() => {
-    const status = searchParams.get("status")
-    const searchQueryCategory = searchParams.get("category")
-    const order = searchParams.get("sortOrder")
-    if(status) {
-      setActiveTab(status)
+    const status = searchParams.get("status");
+    const searchQueryCategory = searchParams.get("category");
+    const order = searchParams.get("sortOrder");
+    if (status) {
+      setActiveTab(status);
     }
-    if(category) {
-      setCategory(searchQueryCategory || "")
+    if (category) {
+      setCategory(searchQueryCategory || "");
     }
-    if(order) {
-      setSortOrder(order)
+    if (order) {
+      setSortOrder(order);
     }
-  }, [searchParams])
-
+  }, [searchParams]);
 
   useEffect(() => {
-    const fetchProducts = async() => {
-      try {
-        const products = await getActiveBlogPosts(currentPage, limitCount, activeTab, sortOrder, category)
-        console.log(products)
-      } catch(err) {
-
+    const fetchProducts = async () => {
+   
+      try {   
+  
+ const posts = await getActiveBlogPosts(
+          currentPage,
+          limitCount,
+          activeTab,
+          sortOrder,
+          category
+        );
+        if (posts.error) {
+          setPostsError(posts.error);
+          setPosts(null)
+        } else {
+          setPosts(posts.data);
+          setPostsError(null);
+        }     
+       
+      } catch (err: any) {
+        setPosts(null)
+        setPostsError(err.message);
       }
-    }
+    };
 
-    fetchProducts()
-  }, [activeTab, currentPage, limitCount, sortOrder, category])
-
-console.log(currentPage, limitCount)
+    fetchProducts();
+  }, [activeTab, currentPage, limitCount, sortOrder, category]);
 
   // section change
   const handleSectionChange = (value: string) => {
@@ -103,8 +118,12 @@ console.log(currentPage, limitCount)
           className={`${styles.tab} ${
             activeTab === "posts" ? styles.active : ""
           }`}
-          onClick={() => {setActiveTab("posts"),  router.push(pathname + '?' + createQueryString('status', "posts"
-          ))}}
+          onClick={() => {
+            setActiveTab("posts"),
+              router.push(
+                pathname + "?" + createQueryString("status", "posts")
+              );
+          }}
         >
           Active Posts
         </button>
@@ -112,8 +131,12 @@ console.log(currentPage, limitCount)
           className={`${styles.tab} ${
             activeTab === "drafts" ? styles.active : ""
           }`}
-          onClick={() => {setActiveTab("drafts"),  router.push(pathname + '?' + createQueryString('status', "drafts"
-          ))}}
+          onClick={() => {
+            setActiveTab("drafts"),
+              router.push(
+                pathname + "?" + createQueryString("status", "drafts")
+              );
+          }}
         >
           Drafts
         </button>
@@ -126,7 +149,9 @@ console.log(currentPage, limitCount)
           value={sortOrder}
           onChange={(e) => {
             setSortOrder(e.target.value);
-            router.push(pathname + "?" + createQueryString("sortOrder", e.target.value));
+            router.push(
+              pathname + "?" + createQueryString("sortOrder", e.target.value)
+            );
           }}
         >
           <option value="desc">Sort by Most Recent</option>
@@ -135,8 +160,10 @@ console.log(currentPage, limitCount)
 
         <select
           className={styles.dropdown}
-          value={category ?  category : ""}
-          onChange={(e) => {handleSectionChange(e.target.value)}}
+          value={category ? category : ""}
+          onChange={(e) => {
+            handleSectionChange(e.target.value);
+          }}
         >
           <option value="all">All</option>
           {categories?.map((category) => (
@@ -147,12 +174,14 @@ console.log(currentPage, limitCount)
 
       {/* posts */}
       <div className={styles.dataContainer}>
+      {featuredPostError && <div>{featuredPostError}</div>}
         {featuredPost && (
-          <div>
-            <h2>Featured Post</h2>
-            <Card post={featuredPost} />
-          </div>
+        <div className={styles.tableCardContainer}>
+          <h2>Featured Post</h2>
+            <TableCard post = {featuredPost}/>
+       </div>
         )}
+        {myPicksError && <div>{myPicksError}</div>}
         {myPicks && (
           <div>
             <h2>My Picks</h2>
@@ -161,6 +190,7 @@ console.log(currentPage, limitCount)
             ))}
           </div>
         )}
+        {postsError && <div>{postsError}</div>}
       </div>
     </div>
   );
