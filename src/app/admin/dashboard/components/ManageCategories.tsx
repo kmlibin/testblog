0c0547a494238f591c2de250de54c9fa0efee6c5
Kelli Libin <km.putnam2@gmail.com>
2025-04-03 16:31:04 -0600

    set up handle delete function

--- src/app/admin/dashboard/components/ManageCategories.tsx
+++ src/app/admin/dashboard/components/ManageCategories.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import styles from "./managecategories.module.css";
-import { CategoryWithId, NewCategory } from "@/app/types";
+import { CategoryWithId } from "@/app/types";
 import { FaEdit, FaTrash, FaCheck } from "react-icons/fa";
 import { RiCloseLargeFill } from "react-icons/ri";
 import InputColor from "react-input-color";
@@ -9,16 +9,20 @@ import CreateNewCategory from "./CreateNewCategory";
 import { uploadImage } from "@/app/utils/uploadImage";
 import { deleteObject, ref } from "firebase/storage";
 import { storage } from "@/app/firebase/config";
-import { editCategory } from "@/app/actions/categories";
+import { editCategory, deleteCategory } from "@/app/actions/categories";
 import Modal from "@/components/modal/Modal";
+import { useRouter } from "next/navigation";
+
+import paths from "@/paths";
 
 type Props = {
   categories: CategoryWithId[] | null;
 };
 
 const ManageCategories = ({ categories: initialCategories }: Props) => {
+  const router = useRouter();
   const [modalMessage, setModalMessage] = useState("");
-  const [isModalOpen, setIsModalOpen] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [newImage, setNewImage] = useState<any>(null);
   const [newCatImage, setNewCatImage] = useState<any>(null);
   const [newCategory, setNewCategory] = useState({
@@ -26,8 +30,8 @@ const ManageCategories = ({ categories: initialCategories }: Props) => {
     color: "",
     image: {
       url: null,
-      file: null
-    }
+      file: null,
+    },
   });
   const [categories, setCategories] = useState<CategoryWithId[]>(
     initialCategories || []
@@ -39,9 +43,6 @@ const ManageCategories = ({ categories: initialCategories }: Props) => {
   const [categoryImages, setCategoryImages] = useState<{ [key: string]: any }>(
     {}
   );
-  const [categoryColors, setCategoryColors] = useState<{
-    [key: string]: string;
-  }>({});
   const [editingCategoryId, setEditingCategoryId] = useState<string | null>(
     null
   );
@@ -52,6 +53,10 @@ const ManageCategories = ({ categories: initialCategories }: Props) => {
   const closeModal = () => {
     console.log("runs");
     setShowModal(false);
+    setIsModalOpen(false);
+    if (success === true) {
+      router.push(paths.adminPage());
+    }
   };
 
   const handleEditClick = (categoryId: string) => {
@@ -86,6 +91,7 @@ const ManageCategories = ({ categories: initialCategories }: Props) => {
     //checks file size
     if (file.size > MAX_FILE_SIZE) {
       alert("File is too large. Please upload an image smaller than 3MB.");
+      return;
     }
 
     console.log("works");
@@ -94,9 +100,9 @@ const ManageCategories = ({ categories: initialCategories }: Props) => {
         ...prev,
         image: {
           url: URL.createObjectURL(file),
-        file: file,
-        }
-      }))
+          file: file,
+        },
+      }));
     } else {
       setNewImage({
         url: URL.createObjectURL(file),
@@ -131,14 +137,7 @@ const ManageCategories = ({ categories: initialCategories }: Props) => {
     });
   };
 
-  const handleOpenModal = () => setIsModalOpen(true);
-  const handleCloseModal = () => setIsModalOpen(false);
-
-  const handlePublishNewCategory = () => {
-    console.log("published");
-  };
-
-  const handlePublishChanges = async (e: any, category: any) => {
+  const handlePublishChanges = async (e: any) => {
     e.preventDefault();
 
     try {
@@ -206,18 +205,47 @@ const ManageCategories = ({ categories: initialCategories }: Props) => {
     }
   };
 
-  console.log("edited Category", editedCategory);
-  //create new category
-  //save name, color, image
+  const handleDeleteCategory = async (e: any, category: any) => {
+    e.preventDefault();
+    if (category.posts?.length >= 1 || category.drafts?.length >= 1) {
+      setSuccess(false);
+      setModalMessage(
+        "Cannot delete a category that has active posts and/or drafts attached to it."
+      );
+      setShowModal(true);
+      return;
+    }
 
-  //edit color, DONE
-  //hook color up to state, save as hex value, also preview it. previous color needs to be in preview.
+    try {
+      const response = await deleteCategory(category);
+      //if successful
+      if (response?.error === false) {
+        const { message } = response;
+        console.log("response", response);
+        // setLoading(false);
+        setSuccess(true);
+        setModalMessage(message);
+        setShowModal(true);
+      }
+      //if unsuccessful
+      if (response?.error === true) {
+        console.log("error ");
+        const { message } = response;
+        // setLoading(false);
+        setSuccess(false);
+        setModalMessage(message);
+        setShowModal(true);
+        console.log(message);
+      }
+    } catch (error: any) {
+      console.log("error adding product");
+      // setLoading(false);
+      setSuccess(false);
+      setModalMessage(error.message);
+      setShowModal(true);
+    }
+  };
 
-  //edit image
-  //prev image needs to be shown, delete/upload to google storage
-
-  //edit name, DONE
-  //prev name needs shown, new name
   return (
     <div>
       {/* Manage Categories Button */}
@@ -255,89 +283,115 @@ const ManageCategories = ({ categories: initialCategories }: Props) => {
                 handleNewFile={handleNewFile}
                 newCategory={newCategory}
                 setNewCategory={setNewCategory}
+                setIsModalOpen={setIsModalOpen}
               />
             )}
 
             {/* existing categories */}
             {categories.map((category) => (
               <div key={category.id} className={styles.categoryCard}>
-                {/* image with upload button when editing */}
-                <div className={styles.imageContainer}>
-                  <img
-                    src={
-                      newImage && editingCategoryId === category.id
-                        ? newImage.url
-                        : categoryImages[category.id]?.url || category.image.url
-                    }
-                    alt={category.name}
-                    className={styles.categoryImage}
-                  />
-                  {editingCategoryId === category.id && (
-                    <input
-                      type="file"
-                      onChange={(e) => handleNewFile(e, false)}
-                    />
-                  )}
-                </div>
-                <div className={styles.categoryDetails}>
-                  {/* editable cat name */}
-                  <div className={styles.categoryName}>
-                    {editingCategoryId === category.id ? (
-                      <input
-                        type="text"
-                        className={styles.inputField}
-                        value={editedCategory?.name || ""}
-                        onChange={(e) =>
-                          handleInputChange("name", e.target.value)
-                        }
-                      />
-                    ) : (
-                      <h4>{category.name}</h4>
-                    )}
+                {editingCategoryId === category.id && (
+                  <div className={styles.buttonContainer}>
+                    <button
+                      className={styles.exitButton}
+                      onClick={() => setEditingCategoryId(null)}
+                    >
+                      Exit Edit
+                    </button>
                   </div>
-
-                  {/* color, shown only when editing */}
-                  <div className={styles.colorSection}>
-                    <p>Select Color</p>
+                )}
+                <div className={styles.infoContainer}>
+                  {/* image with upload button when editing */}
+                  <div className={styles.imageContainer}>
+                    <img
+                      src={
+                        newImage && editingCategoryId === category.id
+                          ? newImage.url
+                          : categoryImages[category.id]?.url ||
+                            category?.image?.url
+                      }
+                      alt={category.name}
+                      className={styles.categoryImage}
+                    />
                     {editingCategoryId === category.id && (
-                      <InputColor
-                        initialValue={category.color}
-                        onChange={(color) => handleColorChange(color)}
+                      <input
+                        type="file"
+                        onChange={(e) => handleNewFile(e, false)}
                       />
                     )}
-                    <div
-                      style={{
-                        width: 50,
-                        height: 50,
-                        borderRadius: "50%",
-                        backgroundColor:
-                          editedCategory?.id === category.id
-                            ? editedCategory.color
-                            : category.color, // Only show edited category's color
-                      }}
-                    />
                   </div>
+                  <div className={styles.categoryDetails}>
+                    {/* editable cat name */}
+                    <div className={styles.categoryName}>
+                      {editingCategoryId === category.id ? (
+                        <input
+                          type="text"
+                          className={styles.inputField}
+                          value={editedCategory?.name || ""}
+                          onChange={(e) =>
+                            handleInputChange("name", e.target.value)
+                          }
+                        />
+                      ) : (
+                        <h4>{category.name}</h4>
+                      )}
+                    </div>
 
-                  {/* buttons */}
-                  <div className={styles.actionButtons}>
-                    {editingCategoryId === category.id ? (
+                    {/* color, shown only when editing */}
+                    <div className={styles.colorSection}>
+                      {editingCategoryId === category.id && <p>Select Color</p>}
+                      {editingCategoryId === category.id && (
+                        <InputColor
+                          initialValue={category.color}
+                          onChange={(color) => handleColorChange(color)}
+                        />
+                      )}
+                      <div
+                        style={{
+                          width: 50,
+                          height: 50,
+                          borderRadius: "50%",
+                          backgroundColor:
+                            editedCategory?.id === category.id
+                              ? editedCategory.color
+                              : category.color, // Only show edited category's color
+                        }}
+                      />
+                    </div>
+
+                    {/* buttons */}
+                    <div className={styles.actionButtons}>
+                      {editingCategoryId === category.id ? (
+                        <span
+                          className={styles.icon}
+                          onClick={(e) => handlePublishChanges(e)}
+                        >
+                          <FaCheck title="Save Changes" />
+                        </span>
+                      ) : (
+                        <span
+                          className={styles.icon}
+                          onClick={() => handleEditClick(category.id)}
+                        >
+                          <FaEdit title="Edit" />
+                        </span>
+                      )}
+
                       <span
                         className={styles.icon}
-                        onClick={(e) => handlePublishChanges(e, category)}
+                        onClick={(e) => handleDeleteCategory(e, category)}
                       >
-                        <FaCheck title="Save Changes" />
+                        <FaTrash title="Delete" />
                       </span>
-                    ) : (
-                      <span
-                        className={styles.icon}
-                        onClick={() => handleEditClick(category.id)}
-                      >
-                        <FaEdit title="Edit" />
-                      </span>
-                    )}
-                    <span className={styles.icon}>
-                      <FaTrash title="Delete" />
-                    </span>
+                    </div>
+                  </div>
+                  <div className={styles.countContainer}>
+                    <p>
+                      Posts: {category?.posts ? category.posts.length : "0"}
+                    </p>
+                    <p>
+                      Drafts: {category?.drafts ? category.drafts.length : "0"}
+                    </p>
                   </div>
                 </div>
               </div>
