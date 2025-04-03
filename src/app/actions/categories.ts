ccff01d6c4bb67093d721d61c929d346c083a194
Kelli Libin <km.putnam2@gmail.com>
2025-04-03 16:30:05 -0600

    created delete function, deleted old photos from storage if new photo present in edit func

--- src/app/actions/categories.ts
+++ src/app/actions/categories.ts
@@ -16,8 +16,53 @@ import {
   getDocs,
 } from "firebase/firestore/lite";
 import { db } from "../firebase/config";
-
+import { storage } from "@/app/firebase/config";
+import { deleteObject, ref } from "firebase/storage";
 import { revalidatePath } from "next/cache";
+
+export async function deleteCategory(category: any) {
+  //confirm there are no active posts or drafts attached to it
+  const catRef = doc(db, "categories", category.id);
+  const catSnapshot = await getDoc(catRef);
+  const categoryToEdit = catSnapshot.data();
+console.log("category to edit", categoryToEdit)
+  if (categoryToEdit?.posts.length >= 1 || categoryToEdit?.drafts.length >= 1) {
+    return {
+      error: true,
+      message:
+        "Cannot delete category with active posts and/or drafts associated with it",
+    };
+  }
+
+  try {
+    //delete category
+    await deleteDoc(doc(db, "categories", category.id));
+
+    //delete image from storage
+    const oldImageRef = ref(storage, category.image.path);
+    deleteObject(oldImageRef).then(() => {
+      console.log("deleted google storage photo");
+    });
+  } catch (error: any) {
+    //return errors, then successes
+    if (error.code === "permission-denied") {
+      return {
+        error: true,
+        message: "Permission denied. Please try again later.",
+      };
+    }
+    return {
+      error: true,
+      message: "Error deleting category in database, try again later",
+    };
+  }
+  //revalidate paths when you figure out rest of pages
+  revalidatePath("/");
+  return {
+    error: false,
+    message: "Successfully deleted category!",
+  };
+}
 
 export async function createCategory(newCat: any) {
   const { color, name, image } = newCat;
@@ -75,26 +120,48 @@ export async function createCategory(newCat: any) {
 
 export async function editCategory(newCat: any) {
   const { color, name, image, id } = newCat;
+
   let newImage;
-  if (image === null) {
-    newImage = { path: "", url: "" };
-  } else {
+  let oldImage;
+
+  if (image != null) {
     newImage = { url: image.url, path: image.path };
   }
+  const normalizedName = name.trim().toLowerCase();
   try {
-    //get previous collection
+    // check if the category name already exists
+    const categoryRef = collection(db, "categories");
+    const queryResult = query(categoryRef, where("name", "==", normalizedName));
+    const querySnapshot = await getDocs(queryResult);
+
+    if (!querySnapshot.empty) {
+      return {
+        error: true,
+        message: "A category with this name already exists.",
+      };
+    }
+    //get previous category
     const catRef = doc(db, "categories", id);
     const catSnapshot = await getDoc(catRef);
     const categoryToEdit = catSnapshot.data();
 
+    oldImage = categoryToEdit?.image;
+
     //TO DO: check if the url/path are different. if so, delete it from google storage
+    // if(newImage) {
+    if (newImage) {
+      const oldImageRef = ref(storage, oldImage.path);
+      deleteObject(oldImageRef).then(() => {
+        console.log("deleted google storage photo");
+      });
+    }
 
     if (catSnapshot.exists()) {
       await updateDoc(catRef, {
         ...categoryToEdit,
         color,
-        name,
-        image: newImage,
+        name: normalizedName,
+        image: newImage ? newImage : oldImage,
       });
       console.log("success");
     }
