c62425554d058803bfc894c64af933500e389275
Kelli Libin <km.putnam2@gmail.com>
2025-04-03 12:27:52 -0600

    create function to add new category

--- src/app/actions/categories.ts
+++ src/app/actions/categories.ts
@@ -11,18 +11,75 @@ import {
   arrayUnion,
   arrayRemove,
   increment,
+  query,
+  where,
+  getDocs,
 } from "firebase/firestore/lite";
 import { db } from "../firebase/config";
 
 import { revalidatePath } from "next/cache";
 
-export async function editCategory( newCat: any) {
+export async function createCategory(newCat: any) {
+  const { color, name, image } = newCat;
+
+  //doublecheck there are values. return if not.
+  if (!name || !color || !image) {
+    return {
+      error: true,
+      message: "Category name , color, and image are required.",
+    };
+  }
+
+  //convert name to lower case, store it lower case, and then upper case it on frontend. makes it easier to check if there are duplicat names
+  const normalizedName = name.trim().toLowerCase();
+  try {
+    // check if the category name already exists
+    const categoryRef = collection(db, "categories");
+    const queryResult = query(categoryRef, where("name", "==", normalizedName));
+    const querySnapshot = await getDocs(queryResult);
+
+    if (!querySnapshot.empty) {
+      return {
+        error: true,
+        message: "A category with this name already exists.",
+      };
+    }
+    //add to categories
+    await addDoc(collection(db, "categories"), {
+      color,
+      image,
+      name: normalizedName,
+      post: [],
+      drafts: [],
+    });
+  } catch (error: any) {
+    //return errors, then successes
+    if (error.code === "permission-denied") {
+      return {
+        error: true,
+        message: "Permission denied. Please try again later.",
+      };
+    }
+    return {
+      error: true,
+      message: "Error adding category to database, try again later",
+    };
+  }
+  //revalidate paths when you figure out rest of pages
+  revalidatePath("/");
+  return {
+    error: false,
+    message: "Successfully added new category!",
+  };
+}
+
+export async function editCategory(newCat: any) {
   const { color, name, image, id } = newCat;
-  let newImage
+  let newImage;
   if (image === null) {
-    newImage = {path: "", url: ""}
+    newImage = { path: "", url: "" };
   } else {
-    newImage = {url: image.url, path: image.path}
+    newImage = { url: image.url, path: image.path };
   }
   try {
     //get previous collection
@@ -30,14 +87,16 @@ export async function editCategory( newCat: any) {
     const catSnapshot = await getDoc(catRef);
     const categoryToEdit = catSnapshot.data();
 
+    //TO DO: check if the url/path are different. if so, delete it from google storage
+
     if (catSnapshot.exists()) {
       await updateDoc(catRef, {
         ...categoryToEdit,
         color,
         name,
-        image : newImage,
+        image: newImage,
       });
-      console.log("success")
+      console.log("success");
     }
   } catch (error: any) {
     console.log(error);
